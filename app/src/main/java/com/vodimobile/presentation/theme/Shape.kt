664200a4c68e7c20b660f31d1dc4d77700c22588
Slava <plumsoftwareofficial@gmail.com>
2024-07-02 14:41:20 +0600

    Presentation layer/App theme [+] Generated android app colors with Theme builder. [+] Created shapes. [+] Added typography. [+] Added string resources for EN and RU languages.

---
+++ app/src/main/java/com/vodimobile/presentation/theme/Shape.kt
@@ -0,0 +1,11 @@
+package com.vodimobile.presentation.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.dp
+
+val Shapes = Shapes(
+    extraSmall = RoundedCornerShape(10.dp),
+    small = RoundedCornerShape(12.dp),
+    medium = RoundedCornerShape(15.dp)
+)
