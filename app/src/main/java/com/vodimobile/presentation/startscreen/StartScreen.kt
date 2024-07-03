e60a688e79303ce19ec01b25f55fa64aa6e09e70
Yulia <yulia.shevtsova2005@gmail.com>
2024-07-03 16:18:09 +0600

    Presentation layer/first app screen [+] Added intents [+] Added ViewModel for StartScreen

--- app/src/main/java/com/vodimobile/presentation/startscreen/StartScreen.kt
+++ app/src/main/java/com/vodimobile/presentation/startscreen/StartScreen.kt
@@ -10,14 +10,10 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -28,7 +24,7 @@ import com.vodimobile.presentation.components.SecondaryButton
 import com.vodimobile.presentation.theme.VodimobileTheme
 
 @Composable
-fun StartScreen() {
+fun StartScreen(startScreenViewModel: StartScreenViewModel) {
     Column(
         Modifier
             .fillMaxWidth()
@@ -48,10 +44,18 @@ fun StartScreen() {
                 .padding(vertical = 58.dp)
                 .size(width = 250.dp, height = 133.33.dp)
         )
-        PrimaryButton(text = stringResource(R.string.requister_str), enabled = true, onClick = {
-
-        })
-        SecondaryButton(text = stringResource(R.string.login_str), enabled = true, onClick = {})
+        PrimaryButton(
+            text = stringResource(R.string.requister_str),
+            enabled = true,
+            onClick = {
+                startScreenViewModel.onIntent(Intent.ClickRegistration)
+            })
+        SecondaryButton(
+            text = stringResource(R.string.login_str),
+            enabled = true,
+            onClick = {
+                startScreenViewModel.onIntent(Intent.ClickLogin)
+            })
     }
 }
 
@@ -61,7 +65,7 @@ fun StartScreen() {
 private fun p() {
     VodimobileTheme {
         Scaffold {
-            StartScreen()
+            StartScreen(startScreenViewModel = StartScreenViewModel())
         }
     }
 }
---
+++ app/src/main/java/com/vodimobile/presentation/startscreen/StartScreenViewModel.kt
@@ -0,0 +1,13 @@
+package com.vodimobile.presentation.startscreen
+
+import androidx.lifecycle.ViewModel
+
+class StartScreenViewModel : ViewModel() {
+
+    fun onIntent(intent: Intent) {
+        when (intent) {
+            Intent.ClickLogin -> {}
+            Intent.ClickRegistration -> {}
+        }
+    }
+}
