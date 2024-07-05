44960997f0650f70521bf3517a27bea78d97e43c
Slava <83229808+plumsoftware@users.noreply.github.com>
2024-07-05 18:15:22 +0600

    Presentation layer/First screen (#4)
    [+] Added close button on first screen.

--- app/src/main/java/com/vodimobile/presentation/startscreen/Intent.kt
+++ app/src/main/java/com/vodimobile/presentation/startscreen/Intent.kt
@@ -3,4 +3,5 @@ package com.vodimobile.presentation.startscreen
 sealed class Intent {
     data object ClickRegistration : Intent()
     data object ClickLogin : Intent()
+    data object CloseClick : Intent()
 }
