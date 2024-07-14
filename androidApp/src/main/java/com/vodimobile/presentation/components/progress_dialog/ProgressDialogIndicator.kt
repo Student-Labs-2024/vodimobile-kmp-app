2beef905214070e7eb8ca5500bbd0c81f0977baa
Slava <plumsoftwareofficial@gmail.com>
2024-07-14 11:37:06 +0600

    presentation layer/progress dialog [+] Added progress dialog.

---
+++ androidApp/src/main/java/com/vodimobile/presentation/components/progress_dialog/ProgressDialogIndicator.kt
@@ -0,0 +1,32 @@
+package com.vodimobile.presentation.components.progress_dialog
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.vodimobile.presentation.ProgressDialog
+
+@Composable
+internal fun BoxScope.ProgressDialogIndicator() {
+    Card(
+        shape = MaterialTheme.shapes.small,
+        modifier = Modifier
+            .size(ProgressDialog.PROGRESS_DIALOG_CARD_DP_SIZE)
+            .align(Alignment.Center),
+    ) {
+        Box(modifier = Modifier.fillMaxSize()) {
+            CircularProgressIndicator(
+                modifier = Modifier
+                    .align(alignment = Alignment.Center)
+                    .padding(all = ProgressDialog.PADDING),
+            )
+        }
+    }
+}
