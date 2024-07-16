fe2f5b3edd6aad2844f3ca802003ae082f812ec4
Sergey Ivanov <126597189+ivn-srg@users.noreply.github.com>
2024-07-16 14:33:01 +0600

    Create FAQ screen [+] Create FAQViewModel [+] Create Question struct and static mockQuestionData [+] Create UI for FAQ screen

--- iosApp/iosApp/Views/Main/ProfileViews/FAQView.swift
+++ iosApp/iosApp/Views/Main/ProfileViews/FAQView.swift
@@ -9,14 +9,82 @@
 import SwiftUI
 
 struct FAQScreenView: View {
+    @State private var expandedIndices: [Bool]
+    
+    private var viewModel: FAQViewModel
+    var attributedTitle: AttributedString {
+        var attributedString = AttributedString("Вопрос-ответ от \"Водимобиль\"")
+        attributedString.font = .header3
+        
+        if let range = attributedString.range(of: "Вопрос-ответ") {
+            attributedString[range].foregroundColor = .blueColor
+        }
+        
+        return attributedString
+    }
+    
+    init() {
+        self.viewModel = FAQViewModel()
+        self.expandedIndices = Array(repeating: false, count: viewModel.listOfQuestions.count)
+    }
+    
     var body: some View {
         VStack {
-            Spacer()
-            Text("Hello, FAQScreen!")
+            ScrollView(.vertical, showsIndicators: false) {
+                VStack(alignment: .leading, spacing: 24) {
+                    Text(attributedTitle)
+                        .font(.header3)
+                    Text("Найдите ответ на любой интересующий Вас вопрос.")
+                        .font(.paragraph4)
+                        .foregroundStyle(Color.grayTextColor)
+                }
                 .frame(maxWidth: .infinity)
-            Spacer()
+                .padding(.vertical, 25)
+                .background(Color.blueBoxColor)
+                .padding(.top, 10)
+                
+                LazyVStack(alignment: .leading) {
+                    ForEach(0..<expandedIndices.count, id: \.self) { index in
+                        DisclosureGroup(
+                            isExpanded: $expandedIndices[index],
+                            content: {
+                                Text(viewModel.listOfQuestions[index].body)
+                                    .font(.paragraph5)
+                                    .multilineTextAlignment(.leading)
+                                    .padding(24)
+                                    .frame(maxWidth: .infinity)
+                                    .background(RoundedRectangle(cornerRadius: 10).fill(Color.grayLightColor))
+                            },
+                            label: {
+                                Text(viewModel.listOfQuestions[index].title)
+                                    .font(.header3)
+                                    .foregroundStyle(Color.black)
+                                    .multilineTextAlignment(.leading)
+                            }
+                        )
+                        .tint(Color.black)
+                        .padding(.top, 25)
+                        .padding(.bottom, 5)
+                        .onChange(of: expandedIndices[index]) { newValue in
+                            withAnimation {
+                                if newValue {
+                                    for i in 0..<expandedIndices.count {
+                                        if i != index {
+                                            expandedIndices[i] = false
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        
+                        if index < expandedIndices.count - 1 {
+                            Divider()
+                        }
+                    }
+                }
+                .padding(.horizontal, 40)
+            }
         }
-        .background(Color.grayLightColor.ignoresSafeArea())
         .navigationBarBackButtonHidden()
         .toolbar {
             CustomToolbar(title: String.ScreenTitles.faqScreenTitle)
