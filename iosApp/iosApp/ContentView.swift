148d758574525c36b315a74970de81316a23cbb9
Sergey Ivanov <126597189+ivn-srg@users.noreply.github.com>
2024-07-17 00:20:43 +0600

    Configure iOS for usage suspend shared function

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -2,15 +2,44 @@ import SwiftUI
 import shared
 
 struct ContentView: View {
-	let greet = Greeting().greet()
+    @ObservedObject private(set) var viewModel: HelloModel
+    
+    var body: some View {
+        VStack {
+            Spacer()
+            Button("Start Receiving") {
+                Task {
+                    await viewModel.startObserving()
+                }
+            }
+            
+            Spacer()
+            Text(viewModel.values).foregroundStyle(.black)
+            Spacer()
+        }
+    }
+}
 
-	var body: some View {
-		Text(greet)
-	}
+extension ContentView {
+    
+    @MainActor
+    class HelloModel: ObservableObject {
+        @Published var values: String = ""
+        
+        
+        func startObserving() async {
+            do {
+                let emittedValues = try await HelloWorldRepositoryImpl().getHelloWorld()
+                values = emittedValues.hello + " " + emittedValues.word
+            } catch {
+                print("Произошла ошибка: \(error.localizedDescription)")
+            }
+        }
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
-	static var previews: some View {
-		ContentView()
-	}
+    static var previews: some View {
+        ContentView(viewModel: ContentView.HelloModel())
+    }
 }
