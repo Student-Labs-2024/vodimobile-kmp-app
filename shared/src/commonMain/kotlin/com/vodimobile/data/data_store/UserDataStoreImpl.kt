e5905208901f1a417fa4416cc4eb05996d6f647d
Slava <plumsoftwareofficial@gmail.com>
2024-07-20 11:44:11 +0600

    kmp module/data store [+] Added method for get user data: repository and its impl, use case.

--- shared/src/commonMain/kotlin/com/vodimobile/data/data_store/UserDataStoreImpl.kt
+++ shared/src/commonMain/kotlin/com/vodimobile/data/data_store/UserDataStoreImpl.kt
@@ -1,4 +1,4 @@
-package com.vodimobile.data
+package com.vodimobile.data.data_store
 
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
@@ -7,6 +7,8 @@ import androidx.datastore.preferences.core.stringPreferencesKey
 import com.vodimobile.domain.repository.data_store.UserDataStore
 import com.vodimobile.domain.model.User
 import com.vodimobile.utils.data_store.Constants
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 
 class UserDataStoreImpl(private val dataStore: DataStore<Preferences>) : UserDataStore {
     override suspend fun editUserData(user: User) {
@@ -19,4 +21,27 @@ class UserDataStoreImpl(private val dataStore: DataStore<Preferences>) : UserDat
         }
     }
 
+    override suspend fun getUserData(): Flow<User> {
+        val userFlow: Flow<User> = dataStore.data.map { preferences ->
+            val fullName =
+                preferences[stringPreferencesKey(Constants.DATA_STORE_USER_FULL_NAME)] ?: ""
+            val email = preferences[stringPreferencesKey(Constants.DATA_STORE_USER_EMAIL)] ?: ""
+            val token = preferences[stringPreferencesKey(Constants.DATA_STORE_USER_TOKEN)] ?: ""
+            val password =
+                preferences[stringPreferencesKey(Constants.DATA_STORE_USER_PASSWORD)] ?: ""
+            val phone = preferences[stringPreferencesKey(Constants.DATA_STORE_USER_PHONE)] ?: ""
+
+            return@map User(
+                fullName = fullName,
+                password = password,
+                token = token,
+                phone = phone,
+                email = email,
+            )
+        }
+
+
+
+        return userFlow
+    }
 }
--- shared/src/commonMain/kotlin/com/vodimobile/domain/repository/data_store/UserDataStore.kt
+++ shared/src/commonMain/kotlin/com/vodimobile/domain/repository/data_store/UserDataStore.kt
@@ -1,7 +1,9 @@
 package com.vodimobile.domain.repository.data_store
 
 import com.vodimobile.domain.model.User
+import kotlinx.coroutines.flow.Flow
 
 interface UserDataStore {
     suspend fun editUserData(user: User)
+    suspend fun getUserData() : Flow<User>
 }
