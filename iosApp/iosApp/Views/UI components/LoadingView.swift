22083d0c768af7d04c07b681e7ffd621a8eb3d8e
Sergey Ivanov <126597189+ivn-srg@users.noreply.github.com>
2024-07-22 19:52:21 +0600

    refactor: Refactor code for UI progress bar
    Improve text fields observing in profile data editing screen
    Separate the progress bar view into separated components

---
+++ iosApp/iosApp/Views/UI components/LoadingView.swift
@@ -0,0 +1,42 @@
+//
+//  LoadingView.swift
+//  iosApp
+//
+//  Created by Sergey Ivanov on 22.07.2024.
+//  Copyright © 2024 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 8, style: .circular)
+                .opacity(0.5)
+                .frame(width: 160, height: 160)
+            ProgressView()
+                .progressViewStyle(.circular)
+                .tint(.white)
+            .scaleEffect(2)
+        }
+    }
+}
+
+struct LoadingModifier: ViewModifier {
+    @Binding var isLoading: Bool
+    
+    func body(content: Content) -> some View {
+        ZStack {
+            content
+            if isLoading {
+                LoadingView()
+            }
+        }
+    }
+}
+
+extension View {
+    func loadingOverlay(isLoading: Binding<Bool>) -> some View {
+        self.modifier(LoadingModifier(isLoading: isLoading))
+    }
+}
