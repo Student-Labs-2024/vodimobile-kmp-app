33022aeb4ba39c1a8b0de0e89dd46dcdea714d8d
Slava <plumsoftwareofficial@gmail.com>
2024-07-22 08:33:44 +0600

    kmp module/DI [+] Added method for get user data for IOS.

--- shared/src/iosMain/kotlin/com/vodimobile/di/KoinHelper.kt
+++ shared/src/iosMain/kotlin/com/vodimobile/di/KoinHelper.kt
@@ -1,15 +1,24 @@
 package com.vodimobile.di
 
+import com.vodimobile.domain.model.User
 import com.vodimobile.domain.storage.data_store.UserDataStoreStorage
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 
 class KoinHelper : KoinComponent {
-    val userDataStoreStorage: UserDataStoreStorage by inject()
-
     companion object {
         fun initKoin() {
             com.vodimobile.di.initKoin()
         }
+    }
+
+    val userDataStoreStorage: UserDataStoreStorage by inject()
+
+    suspend fun getUser(): User {
+        var user: User = User.empty()
+        userDataStoreStorage.getUser().collect { value ->
+            user = value
+        }
+        return user
     }
 }
