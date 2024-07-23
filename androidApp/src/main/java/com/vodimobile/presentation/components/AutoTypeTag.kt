e87c07377bc1f4a3b83891dfadabe30fc8d6d8a6
Sofia <savth2004@gmail.com>
2024-07-23 11:42:00 +0600

    Presentation layer/auto tag list
    [~] Change AutoTypeTag
    [~] Change color in MaterialTheme
    [~] Change shape in MaterialTheme

--- androidApp/src/main/java/com/vodimobile/presentation/components/AutoTypeTag.kt
+++ androidApp/src/main/java/com/vodimobile/presentation/components/AutoTypeTag.kt
@@ -1,12 +1,15 @@
 package com.vodimobile.presentation.components
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.shape.RoundedCornerShape
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.vodimobile.presentation.theme.VodimobileTheme
@@ -18,33 +21,51 @@ fun AutoTypeTag(
     isSelected: Boolean,
     onClick: () -> Unit
 ) {
-    TextButton(
-        modifier = modifier
-            .background(
-                color = if (isSelected) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.onPrimary,
-                shape = RoundedCornerShape(12.dp)
-            ),
-        onClick = onClick
+    Button(
+        modifier = modifier.wrapContentHeight(),
+        onClick = onClick,
+        contentPadding = PaddingValues(horizontal = 20.dp, vertical = 8.dp),
+        shape = MaterialTheme.shapes.medium,
+        colors = ButtonDefaults.buttonColors(
+            containerColor = if (isSelected) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.onPrimary,
+            contentColor = if (isSelected) MaterialTheme.colorScheme.onPrimary else MaterialTheme.colorScheme.primary
+        )
     ) {
         Text(
-            modifier = modifier,
             text = text,
-            color = if (isSelected) MaterialTheme.colorScheme.onPrimary else MaterialTheme.colorScheme.primary
+            style = MaterialTheme.typography.labelMedium,
+            textAlign = TextAlign.Center
         )
     }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_NO)
 @Composable
-fun AutoTagButtonUnselectedPreview() {
+fun AutoTagButtonUnselectedLightPreview() {
     VodimobileTheme(dynamicColor = false) {
         AutoTypeTag(text = "Все авто", isSelected = false, onClick = {})
     }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_NO)
 @Composable
-fun AutoTagButtonSelectedPreview() {
+fun AutoTagButtonSelectedLightPreview() {
+    VodimobileTheme(dynamicColor = false) {
+        AutoTypeTag(text = "Все авто", isSelected = true, onClick = {})
+    }
+}
+
+@Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+fun AutoTagButtonUnselectedNightPreview() {
+    VodimobileTheme(dynamicColor = false) {
+        AutoTypeTag(text = "Все авто", isSelected = false, onClick = {})
+    }
+}
+
+@Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+fun AutoTagButtonSelectedNightPreview() {
     VodimobileTheme(dynamicColor = false) {
         AutoTypeTag(text = "Все авто", isSelected = true, onClick = {})
     }
