235d8ea46e52840bd311d6a72f91338952a292fc
Sergey Ivanov <126597189+ivn-srg@users.noreply.github.com>
2024-07-23 17:49:37 +0600

    refactor: Restore KMPDataStorage class for architecture project style

--- iosApp/iosApp/Views/Authorization/StartScreenView.swift
+++ iosApp/iosApp/Views/Authorization/StartScreenView.swift
@@ -1,33 +1,6 @@
 import SwiftUI
 import shared
 
-@MainActor
-final class KMPDataStorage: ObservableObject {
-    private let repository = UserDataStoreRepositoryImpl(dataStore: CreateDataStore_iosKt.createDataStore())
-    private let newUser = User(
-        fullName: "test testov",
-        password: "12344321",
-        token: "token_test",
-        phone: "+79029994148",
-        email: "rele@df.df"
-    )
-    static let defaultUser = User.companion.empty()
-
-    func editUserData() {
-        Task {
-            try await repository.editUserData(user: newUser)
-        }
-    }
-    
-    func getUser() -> User {
-        var user: User = KMPDataStorage.defaultUser
-        Task {
-            user = try await repository.getUserData()
-        }
-        return user
-    }
-}
-
 struct StartScreenView: View {
     @State private var isButtonEnabled: Bool = true
     @ObservedObject private var dataStorage = KMPDataStorage()
