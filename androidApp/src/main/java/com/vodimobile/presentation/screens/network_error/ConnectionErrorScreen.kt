0c09596e9db9c7ef677dee7a353b93aedfae8c7f
Yulia <yulia.shevtsova2005@gmail.com>
2024-07-23 10:18:13 +0600

    presentation layer/network-error-screen [+] added Internet connection verification

--- androidApp/src/main/java/com/vodimobile/presentation/screens/network_error/ConnectionErrorScreen.kt
+++ androidApp/src/main/java/com/vodimobile/presentation/screens/network_error/ConnectionErrorScreen.kt
@@ -1,5 +1,6 @@
 package com.vodimobile.presentation.screens.network_error
 
+import android.content.res.Configuration
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -8,10 +9,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -19,11 +23,15 @@ import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.vodimobile.android.R
+import com.vodimobile.presentation.LeafScreen
 import com.vodimobile.presentation.components.ErrorItem
 import com.vodimobile.presentation.screens.network_error.store.ConnectionErrorEffect
 import com.vodimobile.presentation.screens.network_error.store.ConnectionErrorIntent
 import com.vodimobile.presentation.theme.VodimobileTheme
+import com.vodimobile.presentation.utils.ConnectionStatus
+import com.vodimobile.presentation.utils.connectivityState
 import kotlinx.coroutines.flow.MutableSharedFlow
+
 
 @Composable
 fun ConnectionErrorScreen(
@@ -31,42 +39,54 @@ fun ConnectionErrorScreen(
     networkErrorEffect: MutableSharedFlow<ConnectionErrorEffect>,
     navHostController: NavHostController
 ) {
-    LaunchedEffect(key1 = Unit) {
-        networkErrorEffect.collect() { effect ->
-            when (effect) {
-                ConnectionErrorEffect.CloseRepetir -> {}
+    Scaffold {
+        val connection by connectivityState()
+        val isConnected = connection === ConnectionStatus.Available
+
+        LaunchedEffect(key1 = Unit) {
+            networkErrorEffect.collect() { effect ->
+                when (effect) {
+                    ConnectionErrorEffect.CloseRepetir -> {
+                        if (isConnected) {
+
+                        } else {
+                        }
+                    }
+                }
             }
         }
-    }
-    Column(
-        Modifier
-            .fillMaxSize()
-            .background(MaterialTheme.colorScheme.background)
-            .padding(horizontal = 16.dp, vertical = 40.dp)
-            .padding(top = 20.dp, bottom = 20.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.SpaceBetween
 
-    ) {
-        ErrorItem(
-            title = stringResource(R.string.connection_error),
-            subtitle = stringResource(R.string.connection_error_subtitle),
+        Column(
+            Modifier
+                .fillMaxSize()
+                .background(MaterialTheme.colorScheme.background)
+                .padding(horizontal = 16.dp, vertical = 60.dp)
+                .padding(it),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceBetween
+
         ) {
-            Image(
-                modifier = Modifier
-                    .padding(vertical = 20.dp)
-                    .size(128.dp),
-                painter = painterResource(id = R.drawable.nointernet),
-                contentDescription = stringResource(R.string.connection_error)
-            )
+            ErrorItem(
+                title = stringResource(R.string.connection_error),
+                subtitle = stringResource(R.string.connection_error_subtitle),
+            ) {
+                Image(
+                    modifier = Modifier
+                        .padding(vertical = 20.dp)
+                        .size(128.dp),
+                    painter = painterResource(id = R.drawable.nointernet),
+                    colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onBackground),
+                    contentDescription = stringResource(R.string.connection_error)
+                )
+            }
         }
     }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_NO)
 @Composable
-fun NetworkErrorScreen() {
-    VodimobileTheme(darkTheme = false,dynamicColor = false) {
+fun NetworkErrorScreenLight() {
+    VodimobileTheme(darkTheme = false, dynamicColor = false) {
         val connectionErrorViewModel = ConnectionErrorViewModel()
         ConnectionErrorScreen(
             onNetworkErrorIntent = connectionErrorViewModel::onIntent,
@@ -75,5 +95,22 @@ fun NetworkErrorScreen() {
         )
     }
 }
+
+@Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+fun NetworkErrorScreenNight() {
+    VodimobileTheme(dynamicColor = false) {
+        val connectionErrorViewModel = ConnectionErrorViewModel()
+        ConnectionErrorScreen(
+            onNetworkErrorIntent = connectionErrorViewModel::onIntent,
+            networkErrorEffect = connectionErrorViewModel.connectionErrorEffect,
+            navHostController = rememberNavController()
+        )
+    }
+}
+
+
+
+
 
 
---
+++ androidApp/src/main/java/com/vodimobile/presentation/utils/NetworkHelper.kt
@@ -0,0 +1,64 @@
+package com.vodimobile.presentation.utils
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.ConnectivityManager.NetworkCallback
+import android.net.Network
+import android.net.NetworkCapabilities
+import android.net.NetworkRequest
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.callbackFlow
+
+val Context.currentConnectivityStatus: ConnectionStatus
+    get() {
+        val connectivityManager =
+            getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        return getCurrentConnectivityStatus(connectivityManager)
+    }
+
+private fun getCurrentConnectivityStatus(
+    connectivityManager: ConnectivityManager
+): ConnectionStatus {
+    val connected = connectivityManager.allNetworks.any { network ->
+        connectivityManager.getNetworkCapabilities(network)
+            ?.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+            ?: false
+    }
+
+    return if (connected) {
+        ConnectionStatus.Available
+    }else{
+        ConnectionStatus.Unavailable
+    }
+}
+
+fun Context.observeConnectivityAsFlow() = callbackFlow {
+    val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+    val callback = NetworkCallback { connectionState -> trySend(connectionState) }
+
+    val networkRequest = NetworkRequest.Builder()
+        .addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+        .build()
+    connectivityManager.registerNetworkCallback(networkRequest, callback)
+
+    val currentState = getCurrentConnectivityStatus(connectivityManager)
+    trySend(currentState)
+
+    awaitClose {
+        connectivityManager.unregisterNetworkCallback(callback)
+    }
+}
+
+fun NetworkCallback(callback: (ConnectionStatus) -> Unit): ConnectivityManager.NetworkCallback {
+    return  object : ConnectivityManager.NetworkCallback(){
+        override fun onAvailable(network: Network) {
+            callback(ConnectionStatus.Available)
+        }
+
+        override fun onLost(network: Network) {
+            callback(ConnectionStatus.Unavailable)
+        }
+    }
+}
+
