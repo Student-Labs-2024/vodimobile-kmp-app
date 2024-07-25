5554d29ae494062fd0e7f0224bf392f5806b2241
Yulia <125740080+Yulia4452873558@users.noreply.github.com>
2024-07-25 11:21:36 +0600

    presentation layer/car-cards-ui (#50)
    - Created BottomCard
    - Created CarsCard
    - added recourses

--- shared/src/commonMain/kotlin/com/vodimobile/domain/model/Car.kt
+++ shared/src/commonMain/kotlin/com/vodimobile/domain/model/Car.kt
@@ -12,4 +12,24 @@ data class Car(
     val deposit: Float?,
     val tariffs: List<Tariff>,
     val images: List<Int>,
-)
+) {
+    companion object{
+        fun empty() : Car {
+            return Car(
+                -1,
+                "Hyundai Solaris",
+                "",
+                -1,
+                null,
+                "",
+                "",
+                null,
+                null,
+                listOf(
+                    Tariff(min = 1, max = 5, cost = 2500f)
+                ),
+                emptyList()
+            )
+        }
+    }
+}
