1e33fda9ba38e20463371972074ab01967c45265
Yulia <yulia.shevtsova2005@gmail.com>
2024-07-31 16:27:58 +0600

    kmp-module/cars-from-site [+] added all cars

--- shared/src/commonMain/kotlin/com/vodimobile/utils/cars/cars.kt
+++ shared/src/commonMain/kotlin/com/vodimobile/utils/cars/cars.kt
@@ -114,3 +114,186 @@ internal val datsun_on_do_1 = Car(
         SharedRes.images.datsun_on_do_1
     )
 )
+
+internal val lexus_nx200_1 = Car(
+    carId = 5,
+    model = SharedRes.strings.name_lexus_nx200,
+    number = "",
+    cityId = 55,
+    year = LocalDate(2018, 1, 1).year,
+    transmission = SharedRes.strings.transmission_name_lexus_nx200_1,
+    wheelDrive = SharedRes.strings.wheel_drive_name_lexus_nx200_1,
+    tankValue = SharedRes.strings.tank_value_name_lexus_nx200_1,
+    deposit = 0f,
+    tariffs = listOf(
+        Tariff(
+            min = 5,
+            max = 10,
+            cost = 2000f
+        )
+    ),
+    images = listOf(
+        SharedRes.images.lexus_nx200_1
+    )
+)
+
+internal val nissan_qashqai_1 = Car(
+    carId = 6,
+    model = SharedRes.strings.name_nissan_qashqai,
+    number = "",
+    cityId = 55,
+    year = LocalDate(2019, 1, 1).year,
+    transmission = SharedRes.strings.transmission_name_nissan_qashqai_1,
+    wheelDrive = SharedRes.strings.wheel_drive_name_nissan_qashqai_1,
+    tankValue = SharedRes.strings.tank_value_name_nissan_qashqai_1,
+    deposit = 0f,
+    tariffs = listOf(
+        Tariff(
+            min = 5,
+            max = 10,
+            cost = 2000f
+        )
+    ),
+    images = listOf(
+        SharedRes.images.nissan_qashqai_1
+    )
+)
+
+internal val kia_rio_x_line_1 = Car(
+    carId = 7,
+    model = SharedRes.strings.name_kia_rio_x_line,
+    number = "",
+    cityId = 55,
+    year = LocalDate(2022, 1, 1).year,
+    transmission = SharedRes.strings.transmission_name_kia_rio_x_line_1,
+    wheelDrive = SharedRes.strings.wheel_drive_name_kia_rio_x_line_1,
+    tankValue = SharedRes.strings.tank_value_name_kia_rio_x_line_1,
+    deposit = 0f,
+    tariffs = listOf(
+        Tariff(
+            min = 5,
+            max = 10,
+            cost = 2000f
+        )
+    ),
+    images = listOf(
+        SharedRes.images.kia_rio_x_line_1
+    )
+)
+
+
+internal val renault_duster_1 = Car(
+    carId = 8,
+    model = SharedRes.strings.name_renault_duster,
+    number = "",
+    cityId = 55,
+    year = LocalDate(2019, 1, 1).year,
+    transmission = SharedRes.strings.transmission_name_renault_duster_1,
+    wheelDrive = SharedRes.strings.wheel_drive_name_renault_duster_1,
+    tankValue = SharedRes.strings.tank_value_name_renault_duster_1,
+    deposit = 0f,
+    tariffs = listOf(
+        Tariff(
+            min = 5,
+            max = 10,
+            cost = 2000f
+        )
+    ),
+    images = listOf(
+        SharedRes.images.renault_duster_png_1
+    )
+)
+
+internal val kia_sportage_1 = Car(
+    carId = 9,
+    model = SharedRes.strings.name_kia_sportage,
+    number = "",
+    cityId = 55,
+    year = LocalDate(2019, 1, 1).year,
+    transmission = SharedRes.strings.transmission_name_kia_sportage_1,
+    wheelDrive = SharedRes.strings.wheel_drive_name_kia_sportage_1,
+    tankValue = SharedRes.strings.tank_value_name_kia_sportage_1,
+    deposit = 0f,
+    tariffs = listOf(
+        Tariff(
+            min = 5,
+            max = 10,
+            cost = 2000f
+        )
+    ),
+    images = listOf(
+        SharedRes.images.kia_sportage_1
+    )
+)
+
+
+internal val skoda_octavia_1 = Car(
+    carId = 9,
+    model = SharedRes.strings.name_skoda_octavia,
+    number = "",
+    cityId = 55,
+    year = LocalDate(2022, 1, 1).year,
+    transmission = SharedRes.strings.transmission_name_skoda_octavia_1,
+    wheelDrive = SharedRes.strings.wheel_drive_name_skoda_octavia_1,
+    tankValue = SharedRes.strings.tank_value_name_skoda_octavia_1,
+    deposit = 0f,
+    tariffs = listOf(
+        Tariff(
+            min = 5,
+            max = 10,
+            cost = 2000f
+        )
+    ),
+    images = listOf(
+        SharedRes.images.skoda_octavia_1
+    )
+)
+
+internal val omoda_s5_1 = Car(
+    carId = 9,
+    model = SharedRes.strings.name_omoda_s5,
+    number = "",
+    cityId = 55,
+    year = LocalDate(2023, 1, 1).year,
+    transmission = SharedRes.strings.transmission_name_omoda_s5_1,
+    wheelDrive = SharedRes.strings.wheel_drive_name_omoda_s5_1,
+    tankValue = SharedRes.strings.tank_value_name_omoda_s5_1,
+    deposit = 0f,
+    tariffs = listOf(
+        Tariff(
+            min = 5,
+            max = 10,
+            cost = 2000f
+        )
+    ),
+    images = listOf(
+        SharedRes.images.omoda_s5_1
+    )
+)
+
+internal val mazda_6_1 = Car(
+    carId = 9,
+    model = SharedRes.strings.name_mazda_6,
+    number = "",
+    cityId = 55,
+    year = LocalDate(2021, 1, 1).year,
+    transmission = SharedRes.strings.transmission_name_mazda_6_1,
+    wheelDrive = SharedRes.strings.wheel_drive_name_mazda_6_1,
+    tankValue = SharedRes.strings.tank_value_name_mazda_6_1,
+    deposit = 0f,
+    tariffs = listOf(
+        Tariff(
+            min = 5,
+            max = 10,
+            cost = 2000f
+        )
+    ),
+    images = listOf(
+        SharedRes.images.mazda_6_1
+    )
+)
+
+
+
+
+
