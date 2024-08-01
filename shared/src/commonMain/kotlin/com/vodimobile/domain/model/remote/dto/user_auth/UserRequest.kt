5569db879b145ae3d9dda5daedff6d9576b8c67f
Slava <plumsoftwareofficial@gmail.com>
2024-08-01 08:54:06 +0600

    feat: Request and response [~] Changed user request and response using local variables.

--- shared/src/commonMain/kotlin/com/vodimobile/domain/model/remote/dto/user_auth/UserRequest.kt
+++ shared/src/commonMain/kotlin/com/vodimobile/domain/model/remote/dto/user_auth/UserRequest.kt
@@ -1,10 +1,11 @@
 package com.vodimobile.domain.model.remote.dto.user_auth
 
+import com.vodimobile.shared.buildkonfig.SharedBuildkonfig
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class UserRequest(
-    val UserName: String,
-    val PasswordHash: String,
+    val UserName: String = SharedBuildkonfig.crm_login,
+    val PasswordHash: String = SharedBuildkonfig.crm_password_hash,
     val LongToken: Boolean = true
 )
--- shared/src/commonMain/kotlin/com/vodimobile/domain/model/remote/dto/user_auth/UserResponse.kt
+++ shared/src/commonMain/kotlin/com/vodimobile/domain/model/remote/dto/user_auth/UserResponse.kt
@@ -6,5 +6,5 @@ import kotlinx.serialization.Serializable
 data class UserResponse(
     val accessToken: String,
     val refreshToken: String,
-    val expires: String
+    val expires: Long
 )
