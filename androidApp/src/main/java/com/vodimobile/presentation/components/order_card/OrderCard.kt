7e11747a62f650b5ef9ef29bdb2f70854677b7b6
Yulia <yulia.shevtsova2005@gmail.com>
2024-08-12 19:25:48 +0600

    feat: Order card [+] added color

---
+++ androidApp/src/main/java/com/vodimobile/presentation/components/order_card/OrderCard.kt
@@ -0,0 +1,19 @@
+package com.vodimobile.presentation.components.order_card
+
+import android.annotation.SuppressLint
+import androidx.compose.material3.Card
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@SuppressLint("ComposeModifierMissing")
+@Composable
+fun OrderCard(
+
+) {
+    Card(
+        modifier = Modifier
+
+    ) {
+
+    }
+}
