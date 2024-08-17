8486a2ed887c1cbd09b8e325d7ebcc29097001df
Slava <plumsoftwareofficial@gmail.com>
2024-08-17 17:48:41 +0600

    feat: Integrate supabase into orders screen [+] Added supabase integration into orders screen.

--- androidApp/src/main/java/com/vodimobile/presentation/screens/orders/OrdersScreen.kt
+++ androidApp/src/main/java/com/vodimobile/presentation/screens/orders/OrdersScreen.kt
@@ -32,13 +32,49 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.vodimobile.android.R
+import com.vodimobile.data.data_store.UserDataStoreRepositoryImpl
+import com.vodimobile.data.repository.crm.CrmRepositoryImpl
+import com.vodimobile.data.repository.supabase.SupabaseRepositoryImpl
 import com.vodimobile.domain.model.order.Order
+import com.vodimobile.domain.storage.crm.CrmStorage
+import com.vodimobile.domain.storage.data_store.UserDataStoreStorage
+import com.vodimobile.domain.storage.supabase.SupabaseStorage
+import com.vodimobile.domain.use_case.crm.CreateBidUseCase
+import com.vodimobile.domain.use_case.crm.GetAllPlacesUseCase
+import com.vodimobile.domain.use_case.crm.GetBidCostUseCase
+import com.vodimobile.domain.use_case.crm.GetCarFreeDateRange
+import com.vodimobile.domain.use_case.crm.GetCarListUseCase
+import com.vodimobile.domain.use_case.crm.GetFreeCarsUseCaSE
+import com.vodimobile.domain.use_case.crm.GetServiceListUseCase
+import com.vodimobile.domain.use_case.crm.GetTariffListUseCase
+import com.vodimobile.domain.use_case.crm.PostNewUserUseCase
+import com.vodimobile.domain.use_case.crm.RefreshTokenUseCase
+import com.vodimobile.domain.use_case.data_store.EditPasswordUseCase
+import com.vodimobile.domain.use_case.data_store.EditUserDataStoreUseCase
+import com.vodimobile.domain.use_case.data_store.GetUserDataUseCase
+import com.vodimobile.domain.use_case.data_store.PreRegisterUserUseCase
+import com.vodimobile.domain.use_case.supabase.GetUserUseCase
+import com.vodimobile.domain.use_case.supabase.InsertUserUseCase
+import com.vodimobile.domain.use_case.supabase.UpdateFullNameUseCase
+import com.vodimobile.domain.use_case.supabase.UpdatePasswordUseCase
+import com.vodimobile.domain.use_case.supabase.UpdatePhoneUseCase
+import com.vodimobile.domain.use_case.supabase.UpdateTokensUseCase
+import com.vodimobile.domain.use_case.supabase.order.GetOrdersUseCase
+import com.vodimobile.domain.use_case.supabase.order.InsertOrderUseCase
+import com.vodimobile.domain.use_case.supabase.order.UpdateCostUseCase
+import com.vodimobile.domain.use_case.supabase.order.UpdateCrmOrderUseCase
+import com.vodimobile.domain.use_case.supabase.order.UpdateNumberUseCase
+import com.vodimobile.domain.use_case.supabase.order.UpdateOrderStatusUseCase
+import com.vodimobile.domain.use_case.supabase.order.UpdatePlaceFinishUseCase
+import com.vodimobile.domain.use_case.supabase.order.UpdatePlaceStartUseCase
+import com.vodimobile.domain.use_case.supabase.order.UpdateServicesUseCase
 import com.vodimobile.presentation.Anim
 import com.vodimobile.presentation.DialogIdentifiers
 import com.vodimobile.presentation.LeafOrdersScreen
@@ -51,6 +87,7 @@ import com.vodimobile.presentation.screens.orders.store.OrderIntent
 import com.vodimobile.presentation.screens.orders.store.OrderState
 import com.vodimobile.presentation.theme.ExtendedTheme
 import com.vodimobile.presentation.theme.VodimobileTheme
+import com.vodimobile.utils.data_store.getDataStore
 import kotlinx.coroutines.flow.MutableSharedFlow
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -104,8 +141,12 @@ fun OrdersScreen(
                             .fillMaxWidth()
                             .padding(start = 16.dp, top = 20.dp, end = 16.dp)
                     ) {
-                        SegmentedButtonList(tags = orderState.value.tags,
-                            onSelected = {})
+                        SegmentedButtonList(
+                            tags = orderState.value.tags,
+                            onSelected = {
+                                orderIntent(OrderIntent.SelectOrders(index = it))
+                            }
+                        )
                     }
                 }
             }) { paddingValues ->
@@ -115,11 +156,8 @@ fun OrdersScreen(
                     modifier = Modifier.padding(paddingValues),
                     horizontalAlignment = Alignment.CenterHorizontally
                 ) {
-                    val visible by remember {
-                        mutableStateOf(true)
-                    }
                     AnimatedVisibility(
-                        visible = visible,
+                        visible = orderState.value.isLoading,
                         enter = fadeIn(animationSpec = tween(Anim.fastAnimDuration)),
                         exit = fadeOut(animationSpec = tween(Anim.fastAnimDuration))
                     ) {
@@ -138,14 +176,29 @@ fun OrdersScreen(
                         alignment = Alignment.Top
                     )
                 ) {
-                    items(orderState.value.orders) { item ->
-                        OrderCard(
-                            orderItem = item,
-                            onClick = {
-                                orderIntent(OrderIntent.InfoOrderClick(order = item))
+                    item {
+                        Column(
+                            modifier = Modifier.fillMaxWidth(),
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            AnimatedVisibility(
+                                visible = orderState.value.isLoading,
+                                enter = fadeIn(animationSpec = tween(Anim.fastAnimDuration)),
+                                exit = fadeOut(animationSpec = tween(Anim.fastAnimDuration))
+                            ) {
+                                SmallProgressDialogIndicator(modifier = Modifier.padding(top = 28.dp))
                             }
-                        )
+                        }
                     }
+                    if (!orderState.value.isLoading)
+                        items(orderState.value.orders) { item ->
+                            OrderCard(
+                                orderItem = item,
+                                onClick = {
+                                    orderIntent(OrderIntent.InfoOrderClick(order = item))
+                                }
+                            )
+                        }
                 }
             }
         }
@@ -156,7 +209,73 @@ fun OrdersScreen(
 @Composable
 private fun OrdersScreenLightPreview() {
     VodimobileTheme(dynamicColor = false) {
-        val orderViewModel = OrderViewModel()
+
+        val crmRepository = CrmRepositoryImpl()
+
+        val crmStorage = CrmStorage(
+            getCarListUseCase = GetCarListUseCase(crmRepository = crmRepository),
+            getTariffListUseCase = GetTariffListUseCase(crmRepository = crmRepository),
+            postNewUserUseCase = PostNewUserUseCase(crmRepository = crmRepository),
+            getAllPlacesUseCase = GetAllPlacesUseCase(crmRepository = crmRepository),
+            refreshTokenUseCase = RefreshTokenUseCase(crmRepository = crmRepository),
+            getServiceListUseCase = GetServiceListUseCase(crmRepository = crmRepository),
+            getFreeCarsUseCaSE = GetFreeCarsUseCaSE(crmRepository = crmRepository),
+            getBidCostUseCase = GetBidCostUseCase(crmRepository = crmRepository),
+            getCarFreeDateRange = GetCarFreeDateRange(crmRepository = crmRepository),
+            createBidUseCase = CreateBidUseCase(crmRepository = crmRepository)
+        )
+
+        val supabaseStorage = SupabaseStorage(
+            getUserUseCase = GetUserUseCase(SupabaseRepositoryImpl()),
+            insertUserUseCase = InsertUserUseCase(SupabaseRepositoryImpl()),
+            updateFullNameUseCase = UpdateFullNameUseCase(SupabaseRepositoryImpl()),
+            updatePasswordUseCase = UpdatePasswordUseCase(SupabaseRepositoryImpl()),
+            updateTokensUseCase = UpdateTokensUseCase(SupabaseRepositoryImpl()),
+            updatePhoneUseCase = UpdatePhoneUseCase(SupabaseRepositoryImpl()),
+            insertOrderUseCase = InsertOrderUseCase(SupabaseRepositoryImpl()),
+            getOrdersUseCase = GetOrdersUseCase(SupabaseRepositoryImpl(), crmStorage),
+            updateOrderStatusUseCase = UpdateOrderStatusUseCase(SupabaseRepositoryImpl()),
+            updateNumberUseCase = UpdateNumberUseCase(SupabaseRepositoryImpl()),
+            updateCrmOrderUseCase = UpdateCrmOrderUseCase(SupabaseRepositoryImpl()),
+            updateServicesUseCase = UpdateServicesUseCase(SupabaseRepositoryImpl()),
+            updateCostUseCase = UpdateCostUseCase(SupabaseRepositoryImpl()),
+            updatePlaceFinishUseCase = UpdatePlaceFinishUseCase(SupabaseRepositoryImpl()),
+            updatePlaceStartUseCase = UpdatePlaceStartUseCase(SupabaseRepositoryImpl())
+        )
+
+        val userDataStoreStorage = UserDataStoreStorage(
+            editUserDataStoreUseCase = EditUserDataStoreUseCase(
+                userDataStoreRepository = UserDataStoreRepositoryImpl(
+                    dataStore = getDataStore(LocalContext.current)
+                )
+            ),
+            getUserDataUseCase = GetUserDataUseCase(
+                userDataStoreRepository = UserDataStoreRepositoryImpl(
+                    dataStore = getDataStore(
+                        LocalContext.current
+                    )
+                )
+            ),
+            preRegisterUserUseCase = PreRegisterUserUseCase(
+                userDataStoreRepository = UserDataStoreRepositoryImpl(
+                    dataStore = getDataStore(
+                        LocalContext.current
+                    )
+                )
+            ),
+            editPasswordUseCase = EditPasswordUseCase(
+                userDataStoreRepository = UserDataStoreRepositoryImpl(
+                    dataStore = getDataStore(
+                        LocalContext.current
+                    )
+                )
+            )
+        )
+
+        val orderViewModel = OrderViewModel(
+            supabaseStorage = supabaseStorage,
+            userDataStoreStorage = userDataStoreStorage
+        )
         OrdersScreen(
             orderIntent = orderViewModel::onIntent,
             orderState = orderViewModel.orderState.collectAsState(
@@ -176,7 +295,72 @@ private fun OrdersScreenLightPreview() {
 @Composable
 private fun OrdersScreenDarkPreview() {
     VodimobileTheme(dynamicColor = false) {
-        val orderViewModel = OrderViewModel()
+        val crmRepository = CrmRepositoryImpl()
+
+        val crmStorage = CrmStorage(
+            getCarListUseCase = GetCarListUseCase(crmRepository = crmRepository),
+            getTariffListUseCase = GetTariffListUseCase(crmRepository = crmRepository),
+            postNewUserUseCase = PostNewUserUseCase(crmRepository = crmRepository),
+            getAllPlacesUseCase = GetAllPlacesUseCase(crmRepository = crmRepository),
+            refreshTokenUseCase = RefreshTokenUseCase(crmRepository = crmRepository),
+            getServiceListUseCase = GetServiceListUseCase(crmRepository = crmRepository),
+            getFreeCarsUseCaSE = GetFreeCarsUseCaSE(crmRepository = crmRepository),
+            getBidCostUseCase = GetBidCostUseCase(crmRepository = crmRepository),
+            getCarFreeDateRange = GetCarFreeDateRange(crmRepository = crmRepository),
+            createBidUseCase = CreateBidUseCase(crmRepository = crmRepository)
+        )
+
+        val supabaseStorage = SupabaseStorage(
+            getUserUseCase = GetUserUseCase(SupabaseRepositoryImpl()),
+            insertUserUseCase = InsertUserUseCase(SupabaseRepositoryImpl()),
+            updateFullNameUseCase = UpdateFullNameUseCase(SupabaseRepositoryImpl()),
+            updatePasswordUseCase = UpdatePasswordUseCase(SupabaseRepositoryImpl()),
+            updateTokensUseCase = UpdateTokensUseCase(SupabaseRepositoryImpl()),
+            updatePhoneUseCase = UpdatePhoneUseCase(SupabaseRepositoryImpl()),
+            insertOrderUseCase = InsertOrderUseCase(SupabaseRepositoryImpl()),
+            getOrdersUseCase = GetOrdersUseCase(SupabaseRepositoryImpl(), crmStorage),
+            updateOrderStatusUseCase = UpdateOrderStatusUseCase(SupabaseRepositoryImpl()),
+            updateNumberUseCase = UpdateNumberUseCase(SupabaseRepositoryImpl()),
+            updateCrmOrderUseCase = UpdateCrmOrderUseCase(SupabaseRepositoryImpl()),
+            updateServicesUseCase = UpdateServicesUseCase(SupabaseRepositoryImpl()),
+            updateCostUseCase = UpdateCostUseCase(SupabaseRepositoryImpl()),
+            updatePlaceFinishUseCase = UpdatePlaceFinishUseCase(SupabaseRepositoryImpl()),
+            updatePlaceStartUseCase = UpdatePlaceStartUseCase(SupabaseRepositoryImpl())
+        )
+
+        val userDataStoreStorage = UserDataStoreStorage(
+            editUserDataStoreUseCase = EditUserDataStoreUseCase(
+                userDataStoreRepository = UserDataStoreRepositoryImpl(
+                    dataStore = getDataStore(LocalContext.current)
+                )
+            ),
+            getUserDataUseCase = GetUserDataUseCase(
+                userDataStoreRepository = UserDataStoreRepositoryImpl(
+                    dataStore = getDataStore(
+                        LocalContext.current
+                    )
+                )
+            ),
+            preRegisterUserUseCase = PreRegisterUserUseCase(
+                userDataStoreRepository = UserDataStoreRepositoryImpl(
+                    dataStore = getDataStore(
+                        LocalContext.current
+                    )
+                )
+            ),
+            editPasswordUseCase = EditPasswordUseCase(
+                userDataStoreRepository = UserDataStoreRepositoryImpl(
+                    dataStore = getDataStore(
+                        LocalContext.current
+                    )
+                )
+            )
+        )
+
+        val orderViewModel = OrderViewModel(
+            supabaseStorage = supabaseStorage,
+            userDataStoreStorage = userDataStoreStorage
+        )
         OrdersScreen(
             orderIntent = orderViewModel::onIntent,
             orderState = orderViewModel.orderState.collectAsState(),
