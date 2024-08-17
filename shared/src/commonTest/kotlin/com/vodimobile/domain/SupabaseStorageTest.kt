91b0bba6156f5d12cf8d2431c4a90ad8509929ad
Yulia <yulia.shevtsova2005@gmail.com>
2024-08-17 14:33:54 +0600

    feat: Order requests [+] Added test case for update start place

--- shared/src/commonTest/kotlin/com/vodimobile/domain/SupabaseStorageTest.kt
+++ shared/src/commonTest/kotlin/com/vodimobile/domain/SupabaseStorageTest.kt
@@ -24,7 +24,13 @@ import com.vodimobile.domain.use_case.supabase.UpdateFullNameUseCase
 import com.vodimobile.domain.use_case.supabase.UpdatePasswordUseCase
 import com.vodimobile.domain.use_case.supabase.UpdatePhoneUseCase
 import com.vodimobile.domain.use_case.supabase.UpdateTokensUseCase
+import com.vodimobile.domain.use_case.supabase.order.UpdateCostUseCase
+import com.vodimobile.domain.use_case.supabase.order.UpdateCrmOrderUseCase
+import com.vodimobile.domain.use_case.supabase.order.UpdateNumberUseCase
 import com.vodimobile.domain.use_case.supabase.order.UpdateOrderStatusUseCase
+import com.vodimobile.domain.use_case.supabase.order.UpdatePlaceFinishUseCase
+import com.vodimobile.domain.use_case.supabase.order.UpdatePlaceStartUseCase
+import com.vodimobile.domain.use_case.supabase.order.UpdateServicesUseCase
 import com.vodimobile.shared.buildkonfig.SharedBuildkonfig
 import kotlinx.coroutines.runBlocking
 import kotlin.test.Test
@@ -139,7 +145,13 @@ class SupabaseStorageTest {
                 updatePhoneUseCase = UpdatePhoneUseCase(supabaseRepository),
                 updateTokensUseCase = UpdateTokensUseCase(supabaseRepository),
                 getOrdersUseCase = GetOrdersUseCase(supabaseRepository, crmStorage),
-                updateOrderStatusUseCase = UpdateOrderStatusUseCase(supabaseRepository)
+                updateOrderStatusUseCase = UpdateOrderStatusUseCase(supabaseRepository),
+                updateNumberUseCase = UpdateNumberUseCase(SupabaseRepositoryImpl()),
+                updateCrmOrderUseCase = UpdateCrmOrderUseCase(SupabaseRepositoryImpl()),
+                updateServicesUseCase = UpdateServicesUseCase(SupabaseRepositoryImpl()),
+                updateCostUseCase = UpdateCostUseCase(SupabaseRepositoryImpl()),
+                updatePlaceFinishUseCase = UpdatePlaceFinishUseCase(SupabaseRepositoryImpl()),
+                updatePlaceStartUseCase = UpdatePlaceStartUseCase(SupabaseRepositoryImpl())
             )
             val orders = supabaseStorage.getOrders(
                 userId = 0,
@@ -177,10 +189,56 @@ class SupabaseStorageTest {
                 updatePhoneUseCase = UpdatePhoneUseCase(supabaseRepository),
                 updateTokensUseCase = UpdateTokensUseCase(supabaseRepository),
                 getOrdersUseCase = GetOrdersUseCase(supabaseRepository, crmStorage),
-                updateOrderStatusUseCase = UpdateOrderStatusUseCase(supabaseRepository)
+                updateOrderStatusUseCase = UpdateOrderStatusUseCase(supabaseRepository),
+                updateNumberUseCase = UpdateNumberUseCase(SupabaseRepositoryImpl()),
+                updateCrmOrderUseCase = UpdateCrmOrderUseCase(SupabaseRepositoryImpl()),
+                updateServicesUseCase = UpdateServicesUseCase(SupabaseRepositoryImpl()),
+                updateCostUseCase = UpdateCostUseCase(SupabaseRepositoryImpl()),
+                updatePlaceFinishUseCase = UpdatePlaceFinishUseCase(SupabaseRepositoryImpl()),
+                updatePlaceStartUseCase = UpdatePlaceStartUseCase(SupabaseRepositoryImpl())
             )
 
             supabaseStorage.updateStatus(userId = 0, orderId = 11, status = "В обработке")
+        }
+    }
+
+    @Test
+    fun updatePlaceStart() {
+        runBlocking {
+            val supabaseRepository = SupabaseRepositoryImpl()
+            val crmRepository = CrmRepositoryImpl()
+
+            val crmStorage = CrmStorage(
+                getCarListUseCase = GetCarListUseCase(crmRepository = crmRepository),
+                getTariffListUseCase = GetTariffListUseCase(crmRepository = crmRepository),
+                postNewUserUseCase = PostNewUserUseCase(crmRepository = crmRepository),
+                getAllPlacesUseCase = GetAllPlacesUseCase(crmRepository = crmRepository),
+                refreshTokenUseCase = RefreshTokenUseCase(crmRepository = crmRepository),
+                getServiceListUseCase = GetServiceListUseCase(crmRepository = crmRepository),
+                getFreeCarsUseCaSE = GetFreeCarsUseCaSE(crmRepository = crmRepository),
+                getBidCostUseCase = GetBidCostUseCase(crmRepository = crmRepository),
+                getCarFreeDateRange = GetCarFreeDateRange(crmRepository = crmRepository),
+                createBidUseCase = CreateBidUseCase(crmRepository = crmRepository)
+            )
+            val supabaseStorage = SupabaseStorage(
+                getUserUseCase = GetUserUseCase(supabaseRepository),
+                insertOrderUseCase = InsertOrderUseCase(supabaseRepository),
+                insertUserUseCase = InsertUserUseCase(supabaseRepository),
+                updateFullNameUseCase = UpdateFullNameUseCase(supabaseRepository),
+                updatePasswordUseCase = UpdatePasswordUseCase(supabaseRepository),
+                updatePhoneUseCase = UpdatePhoneUseCase(supabaseRepository),
+                updateTokensUseCase = UpdateTokensUseCase(supabaseRepository),
+                getOrdersUseCase = GetOrdersUseCase(supabaseRepository, crmStorage),
+                updateOrderStatusUseCase = UpdateOrderStatusUseCase(supabaseRepository),
+                updateNumberUseCase = UpdateNumberUseCase(SupabaseRepositoryImpl()),
+                updateCrmOrderUseCase = UpdateCrmOrderUseCase(SupabaseRepositoryImpl()),
+                updateServicesUseCase = UpdateServicesUseCase(SupabaseRepositoryImpl()),
+                updateCostUseCase = UpdateCostUseCase(SupabaseRepositoryImpl()),
+                updatePlaceFinishUseCase = UpdatePlaceFinishUseCase(SupabaseRepositoryImpl()),
+                updatePlaceStartUseCase = UpdatePlaceStartUseCase(SupabaseRepositoryImpl())
+            )
+
+            supabaseStorage.updatePlaceStart(userId = 2, orderId = 5, placeStart = "В городе")
         }
     }
 }
