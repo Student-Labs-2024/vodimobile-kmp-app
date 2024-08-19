a04ee8d2409f26fbf564694634d0a91caf776ef1
Slava <plumsoftwareofficial@gmail.com>
2024-08-19 23:38:29 +0600

    feat: FCM [-] Removed code to send notification to not to dublicate its functionality.

--- androidApp/src/main/java/com/vodimobile/service/fcm/FirebaseMessagingService.kt
+++ androidApp/src/main/java/com/vodimobile/service/fcm/FirebaseMessagingService.kt
@@ -1,46 +1,13 @@
 package com.vodimobile.service.fcm
 
-import android.app.NotificationManager
-import android.app.PendingIntent
-import android.content.Context
-import android.content.Intent
-import android.media.RingtoneManager
-import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.RemoteMessage
-import com.vodimobile.MainActivity
-import com.vodimobile.android.R
 
 class FirebaseMessagingService : com.google.firebase.messaging.FirebaseMessagingService() {
+    override fun onNewToken(token: String) {
+        super.onNewToken(token)
+    }
 
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
-        if (remoteMessage.data.isNotEmpty()) {
-            val intent = Intent(this, MainActivity::class.java)
-            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-
-            val pendingIntent = PendingIntent.getActivity(
-                this,
-                0,
-                intent,
-                PendingIntent.FLAG_UPDATE_CURRENT
-            )
-            val defaultSoundUri =
-                RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
-
-            val notificationBuilder = NotificationCompat.Builder(
-                this,
-                "com.vodimobile.FirebaseMessagingService"
-            )
-                .setSmallIcon(R.drawable.notification_logo_48x48)
-                .setContentTitle(remoteMessage.notification?.title)
-                .setContentText(if (remoteMessage.notification?.body != null) remoteMessage.notification?.body else "")
-                .setAutoCancel(true)
-                .setPriority(NotificationCompat.PRIORITY_HIGH)
-                .setSound(defaultSoundUri)
-                .setContentIntent(pendingIntent)
-
-            val notificationManager =
-                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-            notificationManager.notify(0, notificationBuilder.build())
-        }
+        super.onMessageReceived(remoteMessage)
     }
 }
