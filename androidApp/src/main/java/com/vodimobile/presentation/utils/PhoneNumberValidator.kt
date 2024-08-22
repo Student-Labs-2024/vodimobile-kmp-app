a8b7afc9ed5863254aeab2156bb8514a0278be43
Sofia <savth2004@gmail.com>
2024-08-22 14:41:02 +0600

    feat: Changed PhoneValidator
    [~] Changed condition to the correct phone number

--- androidApp/src/main/java/com/vodimobile/presentation/utils/PhoneNumberValidator.kt
+++ androidApp/src/main/java/com/vodimobile/presentation/utils/PhoneNumberValidator.kt
@@ -6,11 +6,13 @@ class PhoneNumberValidator {
 
     companion object {
         private const val RU_PHONE_NUMBER_LENGTH = 12
+        private const val RU_CODE_COUNTRY = "+7"
     }
 
     fun isValidPhoneNumber(phoneNumber: String): Boolean {
         return phoneNumber.isNotEmpty() &&
                 phoneNumber.length == RU_PHONE_NUMBER_LENGTH &&
+                phoneNumber.startsWith(RU_CODE_COUNTRY) &&
                 Patterns.PHONE.matcher(phoneNumber).matches()
     }
 }
