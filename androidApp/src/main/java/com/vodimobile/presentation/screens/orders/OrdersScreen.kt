933891a45db77e69a6941c3217ae36c58c281592
Slava <plumsoftwareofficial@gmail.com>
2024-08-24 13:11:30 +0600

    feat: Reservation screen [+] Added coroutine cancellation to optimization.

--- androidApp/src/main/java/com/vodimobile/presentation/screens/orders/OrdersScreen.kt
+++ androidApp/src/main/java/com/vodimobile/presentation/screens/orders/OrdersScreen.kt
@@ -13,9 +13,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -26,12 +24,11 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.State
 import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -93,7 +90,7 @@ import com.vodimobile.utils.data_store.getDataStore
 import kotlinx.coroutines.flow.MutableSharedFlow
 
 @OptIn(ExperimentalMaterial3Api::class)
-@SuppressLint("ComposeModifierMissing", "UnusedMaterialScaffoldPaddingParameter")
+@SuppressLint("ComposeModifierMissing", "UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun OrdersScreen(
     orderIntent: (OrderIntent) -> Unit,
@@ -119,7 +116,14 @@ fun OrdersScreen(
             }
         }
     }
-    orderIntent(OrderIntent.InitCards)
+    SideEffect {
+        orderIntent(OrderIntent.InitCards)
+    }
+    DisposableEffect(key1 = Unit) {
+       onDispose {
+           orderIntent(OrderIntent.CanselAllCoroutines)
+       }
+    }
     ExtendedTheme {
         Scaffold(
             modifier = Modifier
@@ -142,7 +146,7 @@ fun OrdersScreen(
                     Row(
                         modifier = Modifier
                             .fillMaxWidth()
-                            .padding(start = 16.dp, top = 20.dp, end = 16.dp)
+                            .padding(start = 16.dp, end = 16.dp)
                     ) {
                         SegmentedButtonList(
                             tags = orderState.value.tags,
