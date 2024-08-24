a96a3e6d223d4a6627cceaa233bdae800916be46
Slava <83229808+plumsoftware@users.noreply.github.com>
2024-08-24 11:37:57 +0600

    feat: KMP module (#148)
    [~] Added null check to get bid cost request.

--- shared/src/commonMain/kotlin/com/vodimobile/domain/model/remote/dto/bid_cost/BidCostDTO.kt
+++ shared/src/commonMain/kotlin/com/vodimobile/domain/model/remote/dto/bid_cost/BidCostDTO.kt
@@ -5,7 +5,7 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class BidCostDTO(
     val result_code: Int,
-    val cost: Double,
+    val cost: Double?,
     val prepay: Double,
     val deposit: Double,
     val error_message: String?
