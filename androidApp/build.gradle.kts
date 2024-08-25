29b87b13903ce611eee89e6a94312acc948e0635
Slava <plumsoftwareofficial@gmail.com>
2024-08-25 17:48:19 +0600

    feat: Gradle [-] Removed unused gradle plugin.

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.kotlinAndroid)
     alias(libs.plugins.compose.compiler)
-    id("com.google.gms.google-services")
 }
 
 android {
